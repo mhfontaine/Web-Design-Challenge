4fe684119c6caa30b718f06c6df6a312034f0544
mhfontaine <marvin.fontaine@gmail.com>
2020-11-19 18:20:46 -0500

    adding files

---
+++ assets/practice_code.py
@@ -0,0 +1,29 @@
+###############
+#Variables 
+###############
+
+#Assigning a value to an item so that value is stored in the computer's RAM for retrieval later
+
+miles = 10
+type(miles)
+name = 'John'
+type('John')
+a, b, c, = 1, 2, 3
+max_permitted_value = 500
+maxPermittedValue = 500
+
+print(miles)
+print(name)
+print(a)
+print(b)
+print(c)
+print(max_permitted_value)
+print(maxPermittedValue)
+print(a, b, c,)
+
+my_friends = ['Jen', 'Sara', 'Kati']
+print(type(my_friends))
+
+#############
+## Comments
+# ###########
